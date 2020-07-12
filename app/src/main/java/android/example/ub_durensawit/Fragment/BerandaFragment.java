a7fd91538f1901c2cd9a9de6edafa19967e55429
ihyaet <17523126@students.uii.ac.id>
2020-07-12 11:46:25 +0700

    full fragment pengguna keranjang thanks page font compare

--- app/src/main/java/android/example/ub_durensawit/Fragment/BerandaFragment.java
+++ app/src/main/java/android/example/ub_durensawit/Fragment/BerandaFragment.java
@@ -1,10 +1,13 @@
 package android.example.ub_durensawit.Fragment;
 
+import android.content.Intent;
+import android.example.ub_durensawit.CartActivity;
 import android.example.ub_durensawit.R;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +15,24 @@ import androidx.fragment.app.Fragment;
 
 public class BerandaFragment extends Fragment {
 
+
+    private ImageView cartList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_beranda, container,false);
+        View view = inflater.inflate(R.layout.fragment_beranda, container,false);
+
+
+        cartList = view.findViewById(R.id.toCart);
+
+        cartList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), CartActivity.class));
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/android/example/ub_durensawit/ThanksActivity.java
@@ -0,0 +1,28 @@
+package android.example.ub_durensawit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanks);
+
+        //hilangin actionBar
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.AUTOFILL_FLAG_INCLUDE_NOT_IMPORTANT_VIEWS);
+
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
