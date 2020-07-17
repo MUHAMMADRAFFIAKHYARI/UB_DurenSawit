ec126ae3c20590e812d98401d8bf897ce1e0c77f
ihyaet <17523126@students.uii.ac.id>
2020-07-17 09:12:10 +0700

    All item screated, scroll removed in fragmrent pengguna

---
+++ app/src/main/java/android/example/ub_durensawit/AllitemActivity.java
@@ -0,0 +1,14 @@
+package android.example.ub_durensawit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllitemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allitem);
+    }
+}
